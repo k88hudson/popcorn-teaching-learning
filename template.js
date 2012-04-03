f581402fa6604a3c34f1e27895b2261cb3f65034
Kate Hudson <katehudsondesign@gmail.com>
2012-04-03 17:30:03 -0400

    Firefoxnews

--- template.js
+++ template.js
@@ -1,7 +1,7 @@
 document.addEventListener( "DOMContentLoaded", function( e ){
 
   Butter({
-    config: "../config/default.conf",
+    config: "../config/kate.conf",
     ready: function( butter ){
       var media = butter.media[ 0 ];
 
@@ -9,25 +9,39 @@ document.addEventListener( "DOMContentLoaded", function( e ){
         var track = media.addTrack( "Track1" );
         media.addTrack( "Track" + Math.random() );
         media.addTrack( "Track" + Math.random() );
-
+		
+		//These events are created when the template initializes
         var event = track.addTrackEvent({
           type: "text",
           popcornOptions: {
             start: 0,
             end: 3,
             text: "test",
-            target: "Area1"
+            target: "Area2"
           }
         });
 
         butter.tracks[ 2 ].addTrackEvent({ 
           type: "footnote",
           popcornOptions: {
-            start: 1,
-            end: 2,
-            target: "Area2"
+            start: 0,
+            end: 3,
+            target: "Area3"
           }
         });
+
+        butter.tracks[ 1 ].addTrackEvent({ 
+          type: "wikipedia",
+          popcornOptions: {
+            start: 4,
+            end: 7,
+			language: 'en',
+			src: "http://en.wikipedia.org/wiki/Cape_Town",
+			title: "This is wikipedia",
+            target: "Area1"
+          }
+        });
+
       }
 
       media.onReady( start );
